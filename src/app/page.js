80ad7d56c3bd70dfe9bf3c0819f38edce1b96601
javondaniels <155184217+javondaniels@users.noreply.github.com>
2025-05-09 14:06:05 -0400

    Committed all code

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import Game from './App';
+const Page = () => {
+  return (
+    <div>
+      <Game />  
+    </div>
+  );
+};
+
+export default Page;
